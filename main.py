b0b4708bd2bee82242fc1f6a1b900150d3f802d3
peguchi1 <peguchi1@gmail.com>
2020-04-16 23:04:31 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Flask などの必要なライブラリをインポートする
+from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
+
+# 自身の名称を app という名前でインスタンス化する
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    message = "OH MY GODFATHER"
+    return render_template('index.html', message=message)
+
+# /post にアクセスしたときの処理
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    title = "こんにちは"
+    if request.method == 'POST':
+        # リクエストフォームから「名前」を取得して
+        name = request.form['name']
+        # index.html をレンダリングする
+        return render_template('index.html',
+                               name=name, title=title)
+    else:
+        # エラーなどでリダイレクトしたい場合はこんな感じで
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.debug = True # デバッグモード有効化
+    app.run(host='0.0.0.0') # どこからでもアクセス可能に
